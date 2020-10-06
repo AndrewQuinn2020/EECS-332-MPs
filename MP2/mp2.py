167ff6e4df539a0d247e059ec7a18dd284ba0f64
Andrew Quinn <53230903+AndrewQuinn2020@users.noreply.github.com>
2020-10-05 20:35:05 -0400

    Added class documents

--- MP2/mp2.py
+++ MP2/mp2.py
@@ -11,8 +11,14 @@ np.set_printoptions(threshold=sys.maxsize)
 np.set_printoptions(linewidth=1000)
 
 script_dir = os.path.dirname(__file__)
-mids_dir = os.path.join(script_dir, "tests")
+mids_dir = os.path.join(script_dir, "images")
+
+
+
 
 
 if __name__ == "__main__":
     print("Andrew Quinn - EECS 332 - MP#2\n" + ("-" * 80))
+
+    if not os.path.exists(os.path.join(script_dir, 'results')):
+        os.makedirs(os.path.join(script_dir, 'results'))
